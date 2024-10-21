7f032ba6a73c9578d5e50e82adb49356690db28e
RIMON-20 <rimon2022xx@gmail.com>
2024-10-21 07:04:54 +0600

    Update Attitude.js

--- scripts/commands/Attitude.js
+++ scripts/commands/Attitude.js
@@ -21,7 +21,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = [" 𝗥𝗜𝗠𝗢𝗡 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬 "];
+   var hi = ["𝗥𝗜𝗠𝗢𝗡 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
