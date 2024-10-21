a4c32261886abd83c79529f639a40dbc971f4672
RIMON-20 <rimon2022xx@gmail.com>
2024-10-21 06:57:57 +0600

    Update and rename Statesvideo.js to Statusvideo.js

--- scripts/commands/Statusvideo.js
+++ scripts/commands/Statusvideo.js
@@ -1,6 +1,6 @@
 /** I am doing this coding with a lot of difficulty, please don't post it yourself¯\_(ツ)_/¯ **/
 module.exports.config = {
-  name: "states",
+  name: "status",
   version: "1.0.0",
   permission: 0,
   credits: "ALVI",
